ebc73f52c07e9a59b6e4ee21c8b2288c62941a48
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-08-10 06:25:45 +0500

    Create document file

---
+++ pages/_document.js
@@ -0,0 +1,52 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Light.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Regular.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+
+          <link
+            rel="preload"
+            href="/fonts/Poppins-SemiBold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Medium.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Bold.ttf"
+            as="font"
+            crossOrigin="anonymous"
+          ></link>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
